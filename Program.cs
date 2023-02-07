05b04acbaf9ed835d93f5657f140935d4a7e6986
Abrorbek Patidinov <darkorb869@gmail.com>
2023-02-07 18:03:37 +0300

    Sum of Numbers In Digits

--- Program.cs
+++ Program.cs
@@ -15,9 +15,22 @@ namespace Console_C_
       for (int i = 0; i < power; i++) num *= number;
       System.Console.WriteLine("Answer: " + num);   
      }
+       static int SumOfNumbersInDigits() {
+       System.Console.WriteLine("Enter the numbers: "); 
+       int numbers = Convert.ToInt32(Console.ReadLine());
+       int limit = Convert.ToString(numbers).Length;
+       int advance = 0, result = 0;
+       for (int i = 0; i < limit; i++) {
+        advance = numbers - numbers % 10;
+        result = result + (numbers - advance);
+        numbers = numbers / 10;
+       }
+       return result;
+     }
      static void Main(string[] args) 
      {
-       power();
+       int summNumbers = SumOfNumbersInDigits();
+       System.Console.WriteLine("Answer is: " + summNumbers); 
      }
    }
 }
