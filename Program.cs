9d2ba507280c1ca7ad405655e0354aeabf93fb8d
Abrorbek Patidinov <darkorb869@gmail.com>
2023-02-07 17:25:11 +0300

    Power of numbers

--- Program.cs
+++ Program.cs
@@ -6,9 +6,18 @@ namespace Console_C_
 {
    class Program 
    {
+      static void power() {
+      System.Console.WriteLine("Enter the number: ");
+      int number = Convert.ToInt32(Console.ReadLine());
+      System.Console.WriteLine("Enter the power: ");
+      int power = Convert.ToInt32(Console.ReadLine());
+      int num = 1;
+      for (int i = 0; i < power; i++) num *= number;
+      System.Console.WriteLine("Answer: " + num);   
+     }
      static void Main(string[] args) 
      {
-       
+       power();
      }
    }
 }
