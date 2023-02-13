40f51a52768334724b34b74ecb62b2cce540d1a9
Abrorbek Patidinov <darkorb869@gmail.com>
2023-02-13 19:55:08 +0300

    Added commets to each function

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace Console_C_
 {
    class Program 
    {
+      // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
       static void power() {
       System.Console.WriteLine("Enter the number: ");
       int number = Convert.ToInt32(Console.ReadLine());
@@ -14,7 +15,9 @@ namespace Console_C_
       int num = 1;
       for (int i = 0; i < power; i++) num *= number;
       System.Console.WriteLine("Answer: " + num);   
-     }
+     }/////////
+
+       // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
        static int SumOfNumbersInDigits() {
        System.Console.WriteLine("Enter the numbers: "); 
        int numbers = Convert.ToInt32(Console.ReadLine());
@@ -26,7 +29,9 @@ namespace Console_C_
         numbers = numbers / 10;
        }
        return result;
-     }
+     }///////
+     
+     // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
      static void create_array_8() {
       int size = 8;
       int[] array = new int[size];
@@ -35,7 +40,8 @@ namespace Console_C_
         array[i] = random.Next(0, size);
         System.Console.WriteLine(" {0} ", array[i]);
       }
-     }
+     }////////
+     // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве
      static void create_array_with_three_digits() {
        int size = 3, k = 0, sum = 0;
        int[] array = new int[size]; 
@@ -50,7 +56,9 @@ namespace Console_C_
          if (numbers % 2 == 0) { k++; sum += numbers; }
        }
        System.Console.WriteLine("Count of even numbers: {0}", k);
-     }
+     }////////
+
+     // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях
      static void create_array_with_rand_numbers_count_uneven_nums() {
        int size = 3, k = 0, sum = 0;
        int[] array = new int[size];
@@ -65,7 +73,9 @@ namespace Console_C_
         if (numbers % 2 == 1) { k++; sum += numbers; }
        }
        System.Console.WriteLine("Count of uneven numbers: {0}", k);
-     }
+     }///
+
+     // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива
      static void create_array_find_max_and_min_elements() {
        int i, n;
        int[] m = new int[100];
