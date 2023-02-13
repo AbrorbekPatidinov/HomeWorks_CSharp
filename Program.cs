41811f4f185ffb8e995857c4b7b4a254b43a6fd6
Abrorbek Patidinov <darkorb869@gmail.com>
2023-02-13 20:45:25 +0300

    Intersection of two lines, find equation of 'y = k1 * x + b1, y = k2 * x + b2'

--- Program.cs
+++ Program.cs
@@ -105,9 +105,28 @@ namespace Console_C_
       int count = arr.Count(x => x > 0);
       Console.WriteLine($"Кол-во элементов > 0: " + count);
      }
+      // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+      static void intersection_of_two_lines_equations() {
+      Console.Write("Введите k1: ");
+      double k1 = Convert.ToDouble(Console.ReadLine());
+      Console.Write("Введите b1: ");
+      double b1 = Convert.ToDouble(Console.ReadLine());
+      Console.Write("Введите k2: ");
+      double k2 = Convert.ToDouble(Console.ReadLine());
+      Console.Write("Введите b2: ");
+      double b2 = Convert.ToDouble(Console.ReadLine());
+ 
+      double x = -(b1 - b2) / (k1 - k2);
+      double y = k1 * x + b1;
+ 
+      x = Math.Round(x, 3);
+      y = Math.Round(y, 3);
+ 
+      Console.WriteLine($"Intersection of two lines answer: ({x};{y})");
+     }
      static void Main(string[] args) 
      {
-       read_numbers_and_count_if_numbers_higher_than_0();
+       intersection_of_two_lines_equations();
      }
    }
 }
